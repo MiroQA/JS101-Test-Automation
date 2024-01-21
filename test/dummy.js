21356d69296d2dda5f017daf6a64359914495102
Miro Georgiev <mgdreams101@gmail.com>
2024-01-21 04:57:27 +0200

    changed package.json - added type: module changed main to jicer.mjs

---
+++ test/dummy.js
@@ -0,0 +1,12 @@
+import { handleInput } from "../juicer.mjs";
+
+let input = [
+    'Kiwi => 234',
+    'Pear => 2345',
+    'Watermelon => 3456',
+    'Kiwi => 4567',
+    'Pear => 5678',
+    'Watermelon => 6789'
+];
+
+handleInput(input);
